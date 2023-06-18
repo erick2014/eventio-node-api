9a3d7c1c979e915584fecedd521b05f65e1ccfda
Erick Garcia meneses <erickarmandogarcia@hotmail.com>
2023-06-18 10:19:02 -0500

    validate ENVIRONMENT

--- dbs/config/index.js
+++ dbs/config/index.js
@@ -1,12 +1,14 @@
 import '../../dotenv.js'
 
+const {DB_USER,DB_PASS,DB_NAME,SERVER_HOST,DIALECT} = process.env
+
 export default {
   development: {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": process.env.SERVER_HOST,
-    "dialect": process.env.DIALECT
+    "username": DB_USER,
+    "password": DB_PASS,
+    "database": DB_NAME,
+    "host": SERVER_HOST,
+    "dialect": DIALECT
   },
   // Add other environments like test and production if needed
 };

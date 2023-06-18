4a25a485b4aabfde1ce03a2fba10b8dfd85ed844
Erick Garcia meneses <erickarmandogarcia@hotmail.com>
2023-06-18 10:19:13 -0500

    validate ENVIRONMENT

--- dbs/setup.js
+++ dbs/setup.js
@@ -10,11 +10,19 @@ const getInstance = ()=>{
       console.log("returning instance already found...")
       return sequelize
     }
+
+    if(!ENVIRONMENT){
+      throw new Error(`Please setup a valid ENVIRONMENT`)
+    }
+
+    if(!ENVIRONMENT || !(ENVIRONMENT in config)){
+      throw new Error(`Environment ${ENVIRONMENT} is not supported`)
+    }
     console.log('initializing a new connection instance')
     sequelize = new Sequelize(config[ENVIRONMENT]);
     return sequelize
   } catch (error) {
-    console.log("error connecting there..")
+    console.log("error connecting to the db..")
     throw error
   }
 }

8e682d9aed30488e9a2ab265e98219f098af7eac
YESENIA2012 <gyesenia173@gmail.com>
2023-06-20 12:19:12 -0500

    environment variables were set

--- dbs/config/index.js
+++ dbs/config/index.js
@@ -1,15 +1,14 @@
-import '../../dotenv.js'
+import "../../dotenv.js";
 
-const {DB_USER,DB_PASS,DB_NAME,SERVER_HOST,DIALECT} = process.env
+const { DB_USER, DB_PASS, DB_NAME, SERVER_HOST, DIALECT } = process.env;
 
 export default {
   development: {
-    "username": DB_USER,
-    "password": DB_PASS,
-    "database": DB_NAME,
-    "host": SERVER_HOST,
-    "dialect": DIALECT
+    username: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
+    host: SERVER_HOST,
+    dialect: DIALECT,
   },
   // Add other environments like test and production if needed
 };
-

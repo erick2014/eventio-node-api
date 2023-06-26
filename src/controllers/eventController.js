7a67b5041f9ce56a778b6b7d62a8606371eefe4e
YESENIA2012 <gyesenia173@gmail.com>
2023-06-26 15:34:34 -0500

    endpoint for create, update, delete and get all events were implemented

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,22 +1,48 @@
 const Events = require("../models/eventsModel.js");
 
 class EventsController {
-  createEvent({
-    title,
-    description,
-    event_date,
-    event_time,
-    capacity_people_event,
-  }) {
+  getAllEvents() {
+    const allEvents = Events.findAll({ raw: true });
+
+    return allEvents;
+  }
+
+  createEvent({ title, description, event_date, event_time, capacity }) {
     const event = Events.create({
       title,
       description,
       event_date,
       event_time,
-      capacity_people_event,
+      capacity,
     });
 
     return event;
+  }
+
+  async updateEvent(
+    { title, description, event_date, event_time, capacity },
+    eventId
+  ) {
+    await Events.update(
+      {
+        title: title || Events.title,
+        description: description || Events.description,
+        event_date: event_date || Events.event_date,
+        event_time: event_time || Events.event_time,
+        capacity: capacity || Events.capacity,
+      },
+      { where: { id: eventId } }
+    );
+    const eventUpdate = await Events.findOne({ where: { id: eventId } });
+
+    return eventUpdate;
+  }
+
+  deleteEvent(eventId) {
+    const eventToDelete = Events.findOne({ where: { id: eventId } });
+    Events.destroy({ where: { id: eventId } });
+
+    return eventToDelete;
   }
 }
 
--- src/routes/events.js
+++ src/routes/events.js
@@ -1,39 +1,70 @@
 const { Router } = require("express");
 const EventsController = require("../controllers/eventController.js");
-const {
-  createEventSchema,
-  validateRequestEvent,
-} = require("../routes/validateDataEvent.js");
+const { eventSchema, validateRequest } = require("../routes/validateData.js");
 
 const eventRouter = Router();
 const eventsController = new EventsController();
 
-eventRouter.post(
-  "/",
-  validateRequestEvent(createEventSchema),
-  async (req, res, next) => {
-    const {
+eventRouter.get("/", async (_, res, next) => {
+  try {
+    const events = await eventsController.getAllEvents();
+    res.json(events);
+  } catch (error) {
+    next(error);
+  }
+});
+
+eventRouter.post("/", validateRequest(eventSchema), async (req, res, next) => {
+  const { title, description, event_date, event_time, capacity } = req.body;
+
+  try {
+    const newEvent = await eventsController.createEvent({
       title,
       description,
       event_date,
       event_time,
-      capacity_people_event,
-    } = req.body;
+      capacity,
+    });
+
+    res.json(newEvent);
+  } catch (error) {
+    next(error);
+  }
+});
+
+eventRouter.put(
+  "/:id",
+  validateRequest(eventSchema),
+  async (req, res, next) => {
+    const { title, description, event_date, event_time, capacity } = req.body;
+    const eventId = parseInt(req.params.id);
 
     try {
-      const newEvent = await eventsController.createEvent({
-        title,
-        description,
-        event_date,
-        event_time,
-        capacity_people_event,
-      });
+      const eventUpdated = await eventsController.updateEvent(
+        { title, description, event_date, event_time, capacity },
+        eventId
+      );
 
-      res.json(newEvent);
+      if (!eventUpdated) {
+        return res.status(404).json({ error: "Event not found" });
+      }
+
+      res.json(eventUpdated);
     } catch (error) {
       next(error);
     }
   }
 );
+
+eventRouter.delete("/:id", async (req, res, next) => {
+  const eventId = parseInt(req.params.id);
+
+  try {
+    const eventDeleted = await eventsController.deleteEvent(eventId);
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = eventRouter;

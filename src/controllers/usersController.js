30354ae6c802ad53a129a942cfc8f572029d5610
Erick Garcia meneses <erickarmandogarcia@hotmail.com>
2023-06-28 19:32:52 -0500

    setup db for tests

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,22 +2,27 @@ const { Users } = require("../models/usersModel.js");
 
 class UsersController {
   createUser({ firstName, lastName, email, password }) {
+    console.log("here...")
     const user = Users.create({
       firstName,
       lastName,
       email,
       password,
     });
-
+    console.log("user found ? ",user)
     return user;
   }
 
-  loginUser({ email, password }) {
-    const userLogin = Users.findOne({
+  async loginUser({ email, password }) {
+    const userLogin = await Users.findOne({
       where: { email: email, password: password },
     });
-
-    return userLogin;
+    const userAsJson = userLogin.get({plain:true}) 
+    return{
+      firstName: userAsJson.name,
+      lastName: userAsJson.lastName,
+      email: userAsJson.email
+    }
   }
 }
 

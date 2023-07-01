3b8a4a02e9ffa8828f1d389a81722123456a3cb5
YESENIA2012 <gyesenia173@gmail.com>
2023-07-01 17:18:48 -0500

    added deleteUsersInDbCopy function to the usersController controller for delete the data of the table events of the  database eventio_project_test

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,27 +2,36 @@ const { Users } = require("../models/usersModel.js");
 
 class UsersController {
   createUser({ firstName, lastName, email, password }) {
-    console.log("here...")
+    console.log("here...");
     const user = Users.create({
       firstName,
       lastName,
       email,
       password,
     });
-    console.log("user found ? ",user)
+    console.log("user found ? ", user);
     return user;
   }
 
   async loginUser({ email, password }) {
     const userLogin = await Users.findOne({
       where: { email: email, password: password },
+      raw: true,
     });
-    const userAsJson = userLogin.get({plain:true}) 
-    return{
-      firstName: userAsJson.name,
-      lastName: userAsJson.lastName,
-      email: userAsJson.email
+
+    if (!userLogin) {
+      throw new Error("Invalid email or password");
     }
+
+    return {
+      firstName: userLogin.firstName,
+      lastName: userLogin.lastName,
+      email: userLogin.email,
+    };
+  }
+
+  async deleteUsersInDbCopy() {
+    await Users.destroy({ where: {} });
   }
 }
 

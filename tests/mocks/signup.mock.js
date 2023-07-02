3444a59147a291cd224f06dc44b47ee138bf6ce6
YESENIA2012 <gyesenia173@gmail.com>
2023-07-01 21:45:40 -0500

    tests to test the user´s signup were modified

--- tests/mocks/signup.mock.js
+++ tests/mocks/signup.mock.js
@@ -1,11 +1,3 @@
-const mockedCreateUser = {
-  id: 4,
-  firstName: "Charlotte",
-  lastName: "Perez",
-  email: "char@gmail.com",
-  password: "dilan1",
-};
-
 const mockedErrorCreateUser = {
   error:
     '"FirstName" is required, "LastName" is required, "Email" is required, "Password" is required',
@@ -18,7 +10,6 @@ const mockedErrorParamsEmpty = (property) => {
 };
 
 module.exports = {
-  mockedCreateUser,
   mockedErrorCreateUser,
   mockedErrorParamsEmpty,
 };

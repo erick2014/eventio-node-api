1935b94a0289be1bfc4ba9a1376ffe1dbd57fcfe
YESENIA2012 <gyesenia173@gmail.com>
2023-08-28 19:05:54 -0500

    try/catch added to the function

--- src/middlewares/validateIsOwner.js
+++ src/middlewares/validateIsOwner.js
@@ -1,29 +1,31 @@
 const { Events } = require("../models/associations.js");
 
 async function findUserIsOwnerEvent(req, res, next) {
-  const { userId } = req.body
-  const eventId = parseInt(req.params.id);
-
-  let event = await Events.findOne({
-    where: { id: eventId },
-  });
-
-  if (event === null || !event) {
-    const error = new Error("Event not found");
-    error.statusCode = 404;
-    next(error);
-    return
-  }
-  event = event.get({ plain: true });
-
-  if (event.owner_id == userId) {
+  try {
+    const { userId } = req.body
+    const eventId = parseInt(req.params.id);
+  
+    let event = await Events.findOne({
+      where: { id: eventId },
+    });
+  
+    if (event === null || !event) {
+      const error = new Error("Event not found");
+      error.statusCode = 404;
+      throw error
+    }
+    event = event.get({ plain: true });
+  
+    if (event.owner_id != userId) {
+      const error = new Error("This user is not the owner of this event");
+      error.statusCode = 400;
+      throw error 
+    }
     next()
-  } else {
-    const error = new Error("This user is not the owner of this event");
-    error.statusCode = 400;
-    next(error)
-    return 
+  } catch (error) {
+    next(error);
   }
+
 }
 
 module.exports = { findUserIsOwnerEvent }

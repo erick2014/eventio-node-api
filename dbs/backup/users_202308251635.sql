c2940abd6f264a425804a99c64656a9bb7beb50b
YESENIA2012 <gyesenia173@gmail.com>
2023-08-31 14:28:49 -0500

    files suggested was deleted

--- dbs/backup/users_202308251635.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (firstName,lastName,email,password) VALUES
-	 ('Zaina','González','zaina@gmail.com','dilan123'),
-	 ('Yesenia','González','gyese@gmail.com','dilan123'),
-	 ('Dilan','Toloza','d@gmail.com','dilan123');

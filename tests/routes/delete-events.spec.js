82e1e8cc4d02914f65b35e8ee495ed054cfa579f
YESENIA2012 <gyesenia173@gmail.com>
2023-09-03 13:15:57 -0500

    some tests were change

--- tests/routes/delete-events.spec.js
+++ tests/routes/delete-events.spec.js
@@ -48,16 +48,25 @@ describe("Event test", () => {
     });
   });
 
-  it("DELETE /events/ Should return 404 and an error if user isn`t owner event`s ", async () => {
-    const eventId = createdEvent.id;
+  it("DELETE /events/ Should return 400 and error if user isn't owner's event ", async () => {
+    const eventData = {
+      title: "Python",
+      description: "Learn Python",
+      event_date: "25/01/1994",
+      event_time: "20:00PM",
+      capacity: 10,
+      userId: createdEvent.owner_id,
+    };
+    const event = await eventsController.create(eventData);
+    const eventId = event.id;
 
     const response = await request(app)
       .delete(`/events/${eventId}`)
       .send({ userId: 10 });
-
-    expect(response.status).to.equal(404);
+    console.log("response.body", response.body)
+    expect(response.status).to.equal(400) ;
     expect(response.body).to.deep.equal({
-      error: "Event not found",
+      error: "This user is not the owner of this event",
     });
   }); 
 
@@ -70,16 +79,26 @@ describe("Event test", () => {
     expect(response.body).to.deep.equal({ error: "Event not found" });
   });
 
- it("DELETE /events/leave Should return 200 and success: true if an user leaved of an event", async () => {
+  it("DELETE /events/leave Should return 200 and success: true if an user leaved of an event", async () => {
+    const eventData = {
+      title: "Python",
+      description: "Learn Python",
+      event_date: "25/01/1994",
+      event_time: "20:00PM",
+      capacity: 10,
+      userId: createdEvent.owner_id,
+    };
+    const event = await eventsController.create(eventData);
+    const eventId = event.id;
+
     const user2 = await usersController.createUser({
       firstName: "Emma",
       lastName: "González",
       email: "emma2@gmail.com",
       password: "dilan123",
     });
-
-    const eventId = createdEvent.id;
     const userId = user2.id
+
     await eventsController.joinEvent(eventId, userId, false)
 
     const dataToRequest = { userId, eventId }
@@ -123,8 +142,25 @@ describe("Event test", () => {
   }) 
 
   it("DELETE /events/leave Should return 404 and error if the user is event owner", async () => {
-    const userId = createdEvent.owner_id
-    const eventId = createdEvent.id;
+    const user2 = await usersController.createUser({
+      firstName: "Emma",
+      lastName: "González",
+      email: "emma24@gmail.com",
+      password: "dilan123",
+    });
+    const userId = user2.id
+
+    const eventData = {
+      title: "Python",
+      description: "Learn Python",
+      event_date: "25/01/1994",
+      event_time: "20:00PM",
+      capacity: 10,
+      userId: userId,
+    };
+    const event = await eventsController.create(eventData);
+    const eventId = event.id;
+
     const dataToRequest = { userId, eventId }
     const response = await request(app).delete("/events/leave").send(dataToRequest)
     expect(response.status).to.equal(404);

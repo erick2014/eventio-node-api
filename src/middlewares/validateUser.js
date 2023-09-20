1748009181117b1cea11f601d1b77a5fc5ef6e88
YESENIA2012 <gyesenia173@gmail.com>
2023-09-19 23:50:14 -0500

    validateIfUserExist function was implemented, validate if user exist

---
+++ src/middlewares/validateUser.js
@@ -0,0 +1,21 @@
+const { Users } = require("../models/associations.js");
+
+async function validateIfUserExist (req, res, next) {
+  try {
+    const { userId } = req.body;
+    let user = await Users.findOne({ 
+      where : { id: userId }
+    })
+  
+    if(!user){
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error
+    }
+    next()
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { validateIfUserExist }

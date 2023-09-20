c66657dae3a4c0e402f872ef8e6fe3be15604965
YESENIA2012 <gyesenia173@gmail.com>
2023-09-20 12:52:22 -0500

    createUserSchema was modified

--- src/routes/schemas/user.js
+++ src/routes/schemas/user.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const createUserSchema = Joi.object({
   firstName: Joi.string().required().label("FirstName"),
   lastName: Joi.string().required().label("LastName"),
-  email: Joi.string().required().label("Email"),
+  email: Joi.string().email().required().label("Email"),
   password: Joi.string().required().label("Password"),
 });
 

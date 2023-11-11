190b64906bddb4b7f942ed9cecb9e395f5ca4a62
YESENIA2012 <gyesenia173@gmail.com>
2023-11-11 17:00:29 -0500

    validateAccessToken middleware was implemented for token verify

---
+++ src/middlewares/validateAccessToken.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken")
+require("../../dotenv.js")
+
+const verifyToken = (token, secret) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(decoded);
+      }
+    });
+  });
+};
+
+async function validateAccessToken (req, res, next) {
+  const token = req.headers.authorization
+
+  if(token){
+    try {
+      const tokenDecoded = await verifyToken(token, process.env.SECRET) 
+      req.idDecoded = tokenDecoded.idUser;
+      next();
+    } catch (error) {
+      errorObj.statusCode = 403;
+      next(errorObj);
+    }
+  } else {
+    next()
+  }
+}
+
+module.exports = { validateAccessToken }

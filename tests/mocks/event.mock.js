69490eb691008b098386b9eccece088c5888e746
YESENIA2012 <gyesenia173@gmail.com>
2023-11-15 12:20:41 -0500

    mockedErrorParamsEmptyCreateEvent was modified

--- tests/mocks/event.mock.js
+++ tests/mocks/event.mock.js
@@ -1,6 +1,6 @@
 const mockedErrorParamsEmptyCreateEvent = {
   error:
-    '"User Id" is required, "Title" is required, "Description" is required, "Date" is required, "Time" is required, "Capacity" is required',
+    '"Title" is required, "Description" is required, "Date" is required, "Time" is required, "Capacity" is required',
 };
 
 const mockedErrorParamsEmpty = (property) => {

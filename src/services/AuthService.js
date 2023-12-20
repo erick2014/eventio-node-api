9fd0eff5965a63f16fd4a2abd7ce2f8c32237fa5
YESENIA2012 <gyesenia173@gmail.com>
2023-12-20 15:05:39 -0500

    Removed authController.js and validateAccessToken.js files and moved their logic to AuthService.js file

---
+++ src/services/AuthService.js
@@ -0,0 +1,45 @@
+const jwt = require("jsonwebtoken")
+
+const generateAccessToken = async (idUser) => {  
+  try {
+    const userId = { idUser }
+    const tokenAuthentication = await jwt.sign(userId, process.env.SECRET, {expiresIn: "1h"})
+    return tokenAuthentication
+  } catch (error) {
+    const errorObj = new Error(error);
+    errorObj.statusCode = 400;
+    next(errorObj);
+  }
+}
+
+const verifyToken = (token, secret) => {
+return new Promise((resolve, reject) => {
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(decoded);
+    }
+  });
+});
+};
+
+const validateAccessToken = async (req, res, next) => {
+const token = req.headers.authorization
+
+if(token){
+  try {
+    const tokenDecoded = await verifyToken(token, process.env.SECRET) 
+    req.idDecoded = tokenDecoded.idUser;
+    next();
+  } catch (error) {
+    const errorObj = new Error(error);
+    errorObj.statusCode = 403;
+    next(errorObj);
+  }
+} else {
+  next()
+}
+}
+
+module.exports = { generateAccessToken, verifyToken, validateAccessToken };
